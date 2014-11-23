78f6ac4482bd8a100605f029f7248bc0f173c6a4
Jakub Beránek <berykubik@gmail.com>
2014-11-23 20:22:50 +0100

    Přidáno základní nastavení hry (FPS, rozlišení okna).

--- desktop/src/cz/kobzol/turret/desktop/DesktopLauncher.java
+++ desktop/src/cz/kobzol/turret/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import cz.kobzol.turret.TurretGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.foregroundFPS = 60;
+        config.backgroundFPS = 60;
+        config.width = 1200;
+        config.height = 600;
+
 		new LwjglApplication(new TurretGame(), config);
 	}
 }
