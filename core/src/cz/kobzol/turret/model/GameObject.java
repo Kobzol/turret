4bce8070e7288e10985edab5e24145b248711b7a
Jakub Beránek <berykubik@gmail.com>
2014-11-23 17:17:56 +0100

    Přidán klíč ke GameObjectu.

--- core/src/cz/kobzol/turret/model/GameObject.java
+++ core/src/cz/kobzol/turret/model/GameObject.java
@@ -3,18 +3,26 @@ package cz.kobzol.turret.model;
 import cz.kobzol.turret.graphics.IGameObject;
 
 /**
- * Represents game object with unique ID.
+ * Represents game object with unique ID and key.
  */
 public abstract class GameObject implements IGameObject {
     private static int id_counter = 0;
 
     private final int id;
+    private String key;
 
     public GameObject() {
         this.id = GameObject.id_counter++;
+        this.key = "object_" + this.id;
     }
 
-    final public int getId() {
+    public final int getId() {
         return this.id;
+    }
+    public final String getKey() {
+        return this.key;
+    }
+    public final void setKey(String key) {
+        this.key = key;
     }
 }
