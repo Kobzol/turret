af4320a76d2fa44066bb23c74ce321df96651ff5
Jakub Beránek <berykubik@gmail.com>
2014-11-27 21:59:57 +0100

    Cooldownu se nyní zadává čas v milisekundách.

--- core/src/cz/kobzol/turret/util/Cooldown.java
+++ core/src/cz/kobzol/turret/util/Cooldown.java
@@ -1,24 +1,40 @@
 package cz.kobzol.turret.util;
 
 /**
- * Represents time cooldown
+ * Represents time cooldown in milliseconds.
  */
 public class Cooldown {
-    private final long interval;
-    private long value;
+    private final long interval_ms;
+    private double elapsed_s;
 
-    public Cooldown(long interval) {
-        this.interval = interval;
+    public Cooldown(long interval_ms) {
+        this.interval_ms = interval_ms;
+        this.elapsed_s = 0.0;
     }
 
     public boolean isReady() {
-        return this.value > this.interval;
-    }
-    public void reset() {
-        this.value = 0;
+        return (this.elapsed_s * 1000.0) > this.interval_ms;
     }
 
-    public void update() {
-        this.value++;
+    /**
+     * Resets the cooldown if it is ready and returns whether it was resetted or not.
+     * @return True if the cooldown was ready
+     */
+    public boolean resetIfReady() {
+        boolean ready = this.isReady();
+
+        if (ready) {
+            this.reset();
+        }
+
+        return ready;
+    }
+
+    public void reset() {
+        this.elapsed_s = 0.0;
+    }
+
+    public void update(float delta) {
+        this.elapsed_s += delta;
     }
 }
