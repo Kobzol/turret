85e8924e513b190e195e0072ea5f246592f8828a
Jakub Beránek <berykubik@gmail.com>
2014-11-29 20:48:07 +0100

    Přidána věž a menu s věžmi.

---
+++ core/src/cz/kobzol/turret/model/Turret.java
@@ -0,0 +1,10 @@
+package cz.kobzol.turret.model;
+
+import cz.kobzol.turret.graphics.SpriteObject;
+
+/**
+ * Represents a turret.
+ */
+public class Turret extends SpriteObject {
+
+}
---
+++ core/src/cz/kobzol/turret/model/TurretBar.java
@@ -0,0 +1,10 @@
+package cz.kobzol.turret.model;
+
+import cz.kobzol.turret.graphics.SpriteObject;
+
+/**
+ * Turret bar to create turrets.
+ */
+public class TurretBar extends SpriteObject {
+
+}
